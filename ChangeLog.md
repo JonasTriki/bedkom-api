0d8429a80f3c59c95ea78a0aacd94b8664ea76f9
Jonas Triki <trikijonas@gmail.com>
2019-01-05 00:27:44 +0100

    Still WIP, switched to Intellij

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for bedkom-api
+
+## Unreleased changes
--- src/utils/Scrapper.hs
+++ src/utils/Scrapper.hs
@@ -1,26 +1,25 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE QuasiQuotes          #-}
 
 module Scrapper where
 
-import Control.Lens hiding (re)
-import Control.Monad (sequence)
-import Data.Aeson (Array, toJSON)
-import Data.Aeson.Lens (key, nth)
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as C
-import qualified Data.ByteString.Lazy as BL
-import Data.Ini hiding (sections)
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as L
-import Data.Text.Lazy.Builder (toLazyText)
-import Data.Text.Lazy.Encoding
-import HTMLEntities.Decoder
-import Network.Wreq
-import qualified Network.Wreq.Session as Sess
-import Text.HTML.TagSoup
-import Text.RE.TDFA.ByteString.Lazy
+import           Control.Lens                 hiding (re)
+import           Control.Monad                (sequence)
+import           Data.Aeson                   (Array, toJSON)
+import qualified Data.ByteString              as B
+import qualified Data.ByteString.Char8        as C
+import qualified Data.ByteString.Lazy         as BL
+import           Data.Ini                     hiding (sections)
+import qualified Data.Text                    as T
+import qualified Data.Text.Lazy               as L
+import           Data.Text.Lazy.Builder       (toLazyText)
+import           Data.Text.Lazy.Encoding
+import           HTMLEntities.Decoder
+import           Network.Wreq
+import qualified Network.Wreq.Session         as Sess
+import           Text.HTML.TagSoup
+import           Text.RE.TDFA.ByteString.Lazy
 
 readCredentials = do
   iniFile <- readIniFile "me.ini"
@@ -28,23 +27,19 @@ readCredentials = do
       password = iniFile >>= lookupValue "DEFAULT" "password"
   case sequence [feidename, password] of
     Right [fn, pwd] -> return (fn, pwd) -- Is tuple needed?
-    Left err -> error err
+    Left err        -> error err
 
 nameValuePair tag = (fromAttrib "name" tag, fromAttrib "value" tag)
 
-toByteString =
-  encodeUtf8 .
-  toLazyText . htmlEncodedText . L.toStrict . decodeUtf8 . renderTags
+toByteString = encodeUtf8 . toLazyText . htmlEncodedText . L.toStrict . decodeUtf8 . renderTags
 
 --fetchHiddenFields :: Session -> IO [(String, String)]
 fetchHiddenFields sess = do
   getRes <- Sess.get sess "https://fsweb.no/studentweb/login.jsf?inst=FSUIB"
   let extraRe = [re|mojarra\.jsfcljs\(.*{'(.*)':'(.*)'}.*\)|]
-      tags =
-        map nameValuePair $ filter (~== "<input type=hidden>") feideModuleBox
+      tags = map nameValuePair $ filter (~== "<input type=hidden>") feideModuleBox
       feideModuleBox =
-        takeWhile (~/= "</section>") .
-        dropWhile (~/= "<section data-flap-name=feide>") . parseTags $
+        takeWhile (~/= "</section>") . dropWhile (~/= "<section data-flap-name=feide>") . parseTags $
         getRes ^. responseBody
       additional = matches $ toByteString feideModuleBox *=~ extraRe
       fields = tags
