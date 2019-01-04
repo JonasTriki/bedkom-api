02095a907fc16e225b7002fd91ba3782d7eaea16
Jonas Triki <trikijonas@gmail.com>
2019-01-04 21:44:29 +0100

    Scrapper WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Lib
+import Scrapper
 
 main :: IO ()
 main = startApp
--- src/utils/Scrapper.hs
+++ src/utils/Scrapper.hs
@@ -1,11 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Scrapper where
 
-import Network.HTTP
+import Control.Lens hiding (re)
+import Control.Monad (sequence)
+import Data.Aeson (Array, toJSON)
+import Data.Aeson.Lens (key, nth)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy as BL
+import Data.Ini hiding (sections)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as L
+import Data.Text.Lazy.Builder (toLazyText)
+import Data.Text.Lazy.Encoding
+import HTMLEntities.Decoder
+import Network.Wreq
+import qualified Network.Wreq.Session as Sess
+import Text.HTML.TagSoup
+import Text.RE.TDFA.ByteString.Lazy
 
-openURL :: String -> IO String
-openURL x = getResponseBody =<< simpleHTTP (getRequest x)
+readCredentials = do
+  iniFile <- readIniFile "me.ini"
+  let feidename = iniFile >>= lookupValue "DEFAULT" "feidename"
+      password = iniFile >>= lookupValue "DEFAULT" "password"
+  case sequence [feidename, password] of
+    Right [fn, pwd] -> return (fn, pwd) -- Is tuple needed?
+    Left err -> error err
 
-main :: IO ()
-main = do
-  src <- openURL "http://wiki.haskell.org/Haskell"
-  writeFile "temp.htm" src
+nameValuePair tag = (fromAttrib "name" tag, fromAttrib "value" tag)
+
+toByteString =
+  encodeUtf8 .
+  toLazyText . htmlEncodedText . L.toStrict . decodeUtf8 . renderTags
+
+--fetchHiddenFields :: Session -> IO [(String, String)]
+fetchHiddenFields sess = do
+  getRes <- Sess.get sess "https://fsweb.no/studentweb/login.jsf?inst=FSUIB"
+  let extraRe = [re|mojarra\.jsfcljs\(.*{'(.*)':'(.*)'}.*\)|]
+      tags =
+        map nameValuePair $ filter (~== "<input type=hidden>") feideModuleBox
+      feideModuleBox =
+        takeWhile (~/= "</section>") .
+        dropWhile (~/= "<section data-flap-name=feide>") . parseTags $
+        getRes ^. responseBody
+      additional = matches $ toByteString feideModuleBox *=~ extraRe
+      fields = tags
+  print fields
+  print additional
+
+scrap :: IO ()
+scrap = do
+  sess <- Sess.newSession
+  fetchHiddenFields sess
